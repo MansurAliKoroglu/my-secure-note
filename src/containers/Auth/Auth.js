749af5358e2cf96bfa800145f5a00c4758fa79f3
Mansur Ali Koroglu <mansuralikoroglu@gmail.com>
2020-12-25 00:56:36 +0300

    Store email and password inputs data in login page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+
 import classes from './Auth.module.css';
 
 import TextField from '../../components/TextField/TextField';
 
 const Auth = () => {
+  const [emailState, setEmailState] = useState('');
+  const [passwordState, setPasswordState] = useState('');
+
+  const emailChangeHandler = event => {
+    setEmailState(event.target.value);
+  };
+
+  const passwordChangeHandler = event => {
+    setPasswordState(event.target.value);
+  };
+
   return (
     <div className={classes.Auth}>
       <div className={classes.Form}>
@@ -12,13 +25,21 @@ const Auth = () => {
           placeholder="Email"
           type="email"
           autoFocus
+          value={emailState}
+          onChange={emailChangeHandler}
         />
         <TextField
           className={classes.Input}
           placeholder="Password"
           type="password"
+          value={passwordState}
+          onChange={passwordChangeHandler}
         />
-        <button className={classes.LoginButton}>SIGN IN</button>
+        <button
+          className={classes.LoginButton}
+        >
+          SIGN IN
+        </button>
       </div>
     </div>
   );
