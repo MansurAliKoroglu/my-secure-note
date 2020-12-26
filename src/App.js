dba814847c667333463259785de3f216a3cff2f4
Mansur Ali Koroglu <mansuralikoroglu@gmail.com>
2020-12-26 17:38:47 +0300

    Move react redux provider from app to index

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Provider } from "react-redux";
 
 import Auth from './containers/Auth/Auth';
 import Home from './containers/Home/Home';
 import NotFound from './containers/NotFound/NotFound';
-import store from "./store";
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
