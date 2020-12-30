eb008bff789bce4c8fad5aff43698280e602737f
Mansur Ali Koroglu <mansuralikoroglu@gmail.com>
2020-12-30 19:02:24 +0300

    Create select icon animation in sidebar

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,4 +1,7 @@
-import { useEffect } from 'react';
+import {
+  useState,
+  useEffect
+} from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory, useRouteMatch, Switch, Route } from 'react-router-dom';
 
@@ -11,16 +14,69 @@ const Home = () => {
   const history = useHistory();
   const { path } = useRouteMatch();
 
+  let currentPage;
+
+  switch (history.location.pathname) {
+    case '/':
+      currentPage = 'notes';
+      break;
+    case '/create-note':
+      currentPage = 'create-note';
+      break;
+    case '/favorite-notes':
+      currentPage = 'favorite-notes';
+      break;
+    default:
+      throw new Error('There is an error');
+  }
+
+  const [currentPageState, setCurrentPageState] = useState(currentPage);
+
   useEffect(() => {
     if (!isAuthenticated) {
       history.replace('/auth');
     }
   }, [isAuthenticated, history]);
 
+  const sidebarNotesSelectionHandler = () => {
+    if (history.location.pathname === '/') {
+      return;
+    }
+
+    setCurrentPageState('notes');
+
+    history.push(`${path}`);
+  };
+
+  const sidebarAddNoteSelectionHandler = () => {
+    if (history.location.pathname === '/create-note') {
+      return;
+    }
+
+    setCurrentPageState('create-note');
+
+    history.push(`${path}create-note`);
+  };
+
+  const sidebarFavoriteNotesSelectionHandler = () => {
+    if (history.location.pathname === '/favorite-notes') {
+      return;
+    }
+
+    setCurrentPageState('favorite-notes');
+
+    history.push(`${path}favorite-notes`);
+  };
+
   if (isAuthenticated) {
     return (
       <div>
-        <Sidebar />
+        <Sidebar
+          selectedIcon={currentPageState}
+          onNotesSelect={sidebarNotesSelectionHandler}
+          onAddNoteSelect={sidebarAddNoteSelectionHandler}
+          onFavoriteNotesSelect={sidebarFavoriteNotesSelectionHandler}
+        />
         <main className={classes.MainContent}>
           <Switch>
             <Route path={`${path}create-note`}>
--- src/containers/Home/Sidebar/Sidebar.js
+++ src/containers/Home/Sidebar/Sidebar.js
@@ -1,4 +1,9 @@
 import {
+  useRef,
+  useState,
+  useEffect
+} from 'react';
+import {
   FontAwesomeIcon
 } from '@fortawesome/react-fontawesome';
 import {
@@ -10,11 +15,75 @@ import {
 import classes from './Sidebar.module.css';
 
 const Sidebar = props => {
+  const notesIconRef = useRef(null);
+  const addIconRef = useRef(null);
+  const favoriteNotesIconRef = useRef(null);
+
+  const [selectedIndicatorStyleState, setSelectedIndicatorStyleState] = useState(null);
+
+  useEffect(() => {
+    let selectedIconRef;
+
+    switch (props.selectedIcon) {
+      case 'notes':
+        selectedIconRef = notesIconRef;
+        break;
+      case 'create-note':
+        selectedIconRef = addIconRef;
+        break;
+      case 'favorite-notes':
+        selectedIconRef = favoriteNotesIconRef;
+        break;
+      default:
+        throw new Error('Incorrect selected icon');
+    }
+
+    const selectedIndicatorStyle = {
+      height: selectedIconRef.current.offsetHeight,
+      top: selectedIconRef.current.offsetTop
+    };
+
+    setSelectedIndicatorStyleState(selectedIndicatorStyle);
+  }, [props.selectedIcon]);
+
   return (
     <div className={classes.Sidebar}>
-      <FontAwesomeIcon icon={faClipboard} size="lg" className={classes.Icon} />
-      <FontAwesomeIcon icon={faPlusCircle} size="2x" className={[classes.Icon, classes.AddIcon].join(' ')} />
-      <FontAwesomeIcon icon={faStar} size="lg" className={classes.Icon} />
+      <div className={classes.SidebarContent}>
+        <div
+          className={classes.SelectedIndicator}
+          style={selectedIndicatorStyleState}
+        ></div>
+        <div
+          ref={notesIconRef}
+          className={classes.Icon}
+          onClick={props.onNotesSelect}
+        >
+          <FontAwesomeIcon
+            icon={faClipboard}
+            size="lg"
+          />
+        </div>
+        <div
+          ref={addIconRef}
+          className={[classes.Icon, classes.AddIcon].join(' ')}
+          onClick={props.onAddNoteSelect}
+        >
+          <FontAwesomeIcon
+            icon={faPlusCircle}
+            size="2x"
+          />
+        </div>
+        <div
+          ref={favoriteNotesIconRef}
+          className={classes.Icon}
+          onClick={props.onFavoriteNotesSelect}
+        >
+          <FontAwesomeIcon
+            icon={faStar}
+            size="lg"
+          />
+        </div>
+      </div>
     </div>
   );
 };
